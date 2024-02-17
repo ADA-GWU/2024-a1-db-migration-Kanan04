b65c7830e164a85a82dc87e456cbb2f6978efddf
Kanan04 <92312857+Kanan04@users.noreply.github.com>
2024-02-17 23:17:13 +0400

    Add files via upload

---
+++ Setup.sql
@@ -0,0 +1,31 @@
+drop table INTERESTS;
+drop table STUDENTS;
+
+CREATE TABLE STUDENTS (
+    ST_ID SERIAL PRIMARY KEY,
+    ST_NAME VARCHAR(20),
+    ST_LAST VARCHAR(20)
+);
+
+CREATE TABLE INTERESTS (
+    STUDENT_ID INT,
+    INTEREST VARCHAR(20)
+);
+
+INSERT INTO STUDENTS (ST_NAME, ST_LAST) VALUES
+('Konul', 'Gurbanova'),
+('Shahnur', 'Isgandarli'),
+('Natavan', 'Mammadova');
+
+INSERT INTO INTERESTS (STUDENT_ID, INTEREST) VALUES
+(1, 'Tennis'),
+(1, 'Literature'),
+(2, 'Math'),
+(2, 'Tennis'),
+(3, 'Math'),
+(3, 'Music'),
+(2, 'Football'),
+(1, 'Chemistry'),
+(3, 'Chess');
+
+SELECT * FROM STUDENTS;
